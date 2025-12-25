551841ab691c2eeb5b3bb438d1b170b55474b7ba
Sidharth Sankar <sidstuffhere@gmail.com>
2025-12-25 19:12:11 +0530

    Update home.sh

--- home.sh
+++ home.sh
@@ -21,7 +21,6 @@ set -g @tmux2k-theme 'catppuccin'
 run '~/.tmux/plugins/tpm/tpm'
 EOF
 
-export XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 git clone --depth 1 https://github.com/NvChad/starter ~/.config/nvim && rm -rf ~/.config/nvim/.git
 echo 'require("nvchad.configs.lspconfig").defaults()' >| ~/.config/nvim/lua/configs/lspconfig.lua
 curl --output-dir ~/.config/nvim/lua/plugins/ \
@@ -47,6 +46,7 @@ echo "fastfetch" >> ~/.bashrc
 mkdir -p ~/music/playlists
 curl --create-dirs --output-dir ~/.config/mpd \
      -fLO https://raw.githubusercontent.com/sidstuff/setup/master/mpd.conf
+export XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 systemctl --user enable --now mpd.socket
 curl --create-dirs --output-dir ~/.config/ncmpcpp \
      -fLO https://raw.githubusercontent.com/sidstuff/setup/master/config
